4768ba115c1718724a955744254fd54eae261642
GrzeSzk <grz.szkop@gmail.com>
2023-05-27 10:08:09 +0200

    + new validation + corresponding unit tests

--- src/main/java/com/github/grzeszk/medicalclinic/service/PatientService.java
+++ src/main/java/com/github/grzeszk/medicalclinic/service/PatientService.java
@@ -29,6 +29,12 @@ public class PatientService {
         if (entity.isPresent()) {
             throw new IncorrectPatientDataException("Patient already exists");
         }
+        if (patient.getIdCardNo() == null) {
+            throw new IncorrectPatientDataException("ID card number can not be empty!");
+        }
+        if (patient.getPassword().length() < 3) {
+            throw new IncorrectPatientDataException("Password must have more than 3 characters");
+        }
         return patientRepository.addPatient(patient);
     }
 
@@ -39,14 +45,30 @@ public class PatientService {
     }
 
     public Patient updatePatientByEmail(String email, Patient patientEditInfo) {
+        validatePatientEditData(patientEditInfo);
         Patient patient = patientRepository.getPatientByEmail(email)
                 .orElseThrow(() -> new IncorrectPatientDataException("Patient not found"));
+        if (!patient.getIdCardNo().equals(patientEditInfo.getIdCardNo())) {
+            throw new IncorrectPatientDataException("Patient can't change ID card number");
+        }
         return patientRepository.updatePatient(patient, patientEditInfo);
     }
 
     public Patient updatePatientPassword(String email, String password) {
         Patient patient = patientRepository.getPatientByEmail(email)
-                .orElseThrow(() -> new IncorrectPatientDataException());
+                .orElseThrow(IncorrectPatientDataException::new);
+        if (password == null || password.length() < 3) {
+            throw new IncorrectPatientDataException("Password is empty or has got less than 3 characters");
+        }
         return patientRepository.updatePatientPassword(patient, password);
+    }
+
+    public void validatePatientEditData(Patient patientEditInfo) {
+        if (patientEditInfo.getPassword() == null || patientEditInfo.getEmail() == null || patientEditInfo.getBirthday() == null
+                || patientEditInfo.getPhoneNumber() == null || patientEditInfo.getFirstName() == null
+                || patientEditInfo.getLastName() == null || patientEditInfo.getIdCardNo() == null
+                || patientEditInfo.getPassword().length() < 3) {
+            throw new IncorrectPatientDataException("Invalid patient data");
+        }
     }
 }
